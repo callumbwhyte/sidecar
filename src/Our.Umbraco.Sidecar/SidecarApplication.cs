23ba987afbcebd20805dfd05a846dc6bcf7e36c2
Callum Whyte <hey@callumwhyte.com>
2020-02-04 10:05:16 +0000

    Override logger in Umbraco CoreRuntime

---
+++ src/Our.Umbraco.Sidecar/SidecarApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Umbraco.Core;
+using Umbraco.Web;
+
+namespace Our.Umbraco.Sidecar
+{
+    public class SidecarApplication : UmbracoApplicationBase
+    {
+        public void Start()
+        {
+            base.Application_Start(this, new EventArgs());
+        }
+
+        protected override IRuntime GetRuntime() => new SidecarRuntime();
+    }
+}
---
+++ src/Our.Umbraco.Sidecar/SidecarRuntime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Serilog;
+using Umbraco.Core.Logging.Serilog;
+using Umbraco.Core.Runtime;
+using ILogger = Umbraco.Core.Logging.ILogger;
+
+namespace Our.Umbraco.Sidecar
+{
+    internal class SidecarRuntime : CoreRuntime
+    {
+        /// <summary>
+        /// Override logger because <see cref="LoggerConfigExtensions.MinimalConfiguration(LoggerConfiguration)"/> fails
+        /// HttpRuntime.AppDomainId is null outside of a web context
+        /// Http request log enrichers are not needed
+        /// </summary>
+        protected override ILogger GetLogger()
+        {
+            Environment.SetEnvironmentVariable("BASEDIR", AppDomain.CurrentDomain.BaseDirectory, EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("MACHINENAME", Environment.MachineName, EnvironmentVariableTarget.Process);
+
+            var logConfig = new LoggerConfiguration();
+
+            logConfig.MinimumLevel.Verbose()
+                .Enrich.WithProcessId()
+                .Enrich.WithProcessName()
+                .Enrich.WithThreadId()
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .ReadFromConfigFile()
+                .ReadFromUserConfigFile();
+
+            return new SerilogLogger(logConfig);
+        }
+    }
+}
