5d67f540f52b9d7a4b77e4a617ec4682a7116eef
Callum Whyte <hey@callumwhyte.com>
2020-02-23 17:59:09 +0000

    Adding sample console app

---
+++ src/Sidecar.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Our.Umbraco.Sidecar;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Services;
+
+namespace Sidecar.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initialzing...");
+
+            var application = new SidecarApplication();
+
+            application.Start();
+
+            Console.WriteLine("Umbraco loaded successfully");
+
+            var contentService = Current.Services.ContentService;
+
+            var contentItems = contentService.GetRootContent();
+
+            var mediaService = Current.Services.MediaService;
+
+            var mediaItems = mediaService.GetRootMedia();
+
+            Console.ReadLine();
+        }
+    }
+}
